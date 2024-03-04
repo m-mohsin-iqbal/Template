049eefa6d8e0995ac03a69bb7d9f87d10fc9176f
Mohsin Iqbal <m.mohsin.iqbal@outlook.com>
2024-03-04 17:08:16 +0500

    few links updated and schema updation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
-import Script from 'next/script';
-import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+import Header from '@/components/layout/header';
+import Footer from '@/components/layout/footer';
+
+import { Providers } from '@/lib/providers';
+
 
 import './globals.css';
-import Header from '@/components/layout/header';
-import { Providers } from '@/lib/providers';
-import Footer from '@/components/layout/footer';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
 const title = 'Mohsin Iqbal | Full Stack Developer From Lahore, Pakistan.';
 const description =
-  'A self-proclaimed designer who specializes in full stack development (React.js & Node.js), from Ahmedabad, India.';
-const url = 'https://sagarshah.dev';
+  'A self-proclaimed developer who specializes in full stack development (React/Next.js & Node.js), from Lahore, Pakistan.';
+const url = 'https://mohsindev.tech';
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
@@ -24,6 +26,7 @@ export const metadata: Metadata = {
     'Full Stack Developer',
     'React Developer',
     'Next.js Developer',
+    'Node.js Developer'
   ],
   creator: 'Mohsin Iqbal',
   themeColor: [
@@ -38,7 +41,7 @@ export const metadata: Metadata = {
     siteName: title,
     images: [
       {
-        url: '/images/open-graph-sagar.png',
+        url: 'https://full-pose.s3.amazonaws.com/open-graph-mohsin.png',
       },
     ],
   },
@@ -46,8 +49,8 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title,
     description,
-    creator: '@shahsagarm',
-    images: '/images/open-graph-sagar.png',
+    creator: '@MohsinIqbal',
+    images: 'https://full-pose.s3.amazonaws.com/open-graph-mohsin.png',
   },
   icons: {
     icon: '/favicon.ico',
@@ -56,8 +59,6 @@ export const metadata: Metadata = {
   },
 };
 
-const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
-
 export default function RootLayout({
   children,
 }: {
@@ -65,23 +66,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-      {googleAnalyticsId ? (
-        <head>
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-          ></Script>
-          <Script id="google-anayltics-script">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${googleAnalyticsId}');
-          `}
-          </Script>
-        </head>
-      ) : null}
       <body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
         <Providers>
           <Header />
