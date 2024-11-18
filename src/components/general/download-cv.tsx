353ea8e469b0c81a6b9a6ba316ccbb7d4a635dc6
Mohsin Iqbal <m.mohsin.iqbal@outlook.com>
2024-11-18 19:16:49 +0500

    cv link updated

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -5,7 +5,7 @@ import Button from '@/components/general/button';
 const DownloadCV = () => {
   return (
     <Button>
-      <a href="https://mohsin-resume.s3.ap-south-1.amazonaws.com/Mohsin-Iqbal-Resume.pdf" target='_blank'>
+      <a href="https://mohsin-resume.s3.ap-south-1.amazonaws.com/Mohsin-iqbal-Resume-MERN.pdf" target='_blank'>
       Download CV
       </a>
     </Button>
